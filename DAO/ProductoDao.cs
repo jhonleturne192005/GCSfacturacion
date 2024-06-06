9eff71bb9383219a930cfd3f982476913988ee1f
jhonleturne192005 <126303974+jhonleturne192005@users.noreply.github.com>
2024-06-06 10:15:49 -0500

    Update ProductoDao.cs

--- DAO/ProductoDao.cs
+++ DAO/ProductoDao.cs
@@ -1,4 +1,4 @@
-﻿using SistemaFacturacion.Utencilios;
+using SistemaFacturacion.Utencilios;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -6,6 +6,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SistemaFacturacion.DAO
 {
@@ -17,30 +18,7 @@ namespace SistemaFacturacion.DAO
             conexion = new Conexion();
         }
 
-        public DataTable listarProductos()
-        {
-            DataTable dataTable = new DataTable();
-            try
-            {
-                conexion.AbrirConexion();
-
-                SqlDataAdapter dataAdapter = new SqlDataAdapter();
-                SqlCommand cmd = new SqlCommand("sp_listar_productos", conexion.ConexionSQL);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                dataAdapter.SelectCommand = cmd;
-                dataAdapter.Fill(dataTable);
-
-                conexion.CerrarConexion();
-            }
-            catch(Exception e)
-            {
-
-            }
-
-            return dataTable;
-        }
-
+ 
         public DataTable listarProductos(int numero_pagina, int numero_elementos)
         {
             DataTable dataTable = new DataTable();
@@ -49,9 +27,10 @@ namespace SistemaFacturacion.DAO
                 conexion.AbrirConexion();
 
                 SqlDataAdapter dataAdapter = new SqlDataAdapter();
-                SqlCommand cmd = new SqlCommand("sp_listar_productos", conexion.ConexionSQL);
+                SqlCommand cmd = new SqlCommand("sp_paginacion_listar_productos", conexion.ConexionSQL);
                 cmd.CommandType = CommandType.StoredProcedure;
-
+                cmd.Parameters.AddWithValue("@numero_pagina", numero_pagina);
+                cmd.Parameters.AddWithValue("@numero_elementos", numero_elementos);
                 dataAdapter.SelectCommand = cmd;
                 dataAdapter.Fill(dataTable);
 
@@ -59,7 +38,7 @@ namespace SistemaFacturacion.DAO
             }
             catch (Exception e)
             {
-
+                MessageBox.Show(e.Message);
             }
 
             return dataTable;
@@ -84,9 +63,8 @@ namespace SistemaFacturacion.DAO
             }
             catch (Exception e)
             {
-
+                MessageBox.Show(e.Message);
             }
-
             return dataTable;
         }
 
@@ -101,7 +79,6 @@ namespace SistemaFacturacion.DAO
                 SqlCommand cmd = new SqlCommand("sp_buscar_producto", conexion.ConexionSQL);
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@texto_buscar", texto_buscar);
-
                 dataAdapter.SelectCommand = cmd;
                 dataAdapter.Fill(dataTable);
 
@@ -109,10 +86,122 @@ namespace SistemaFacturacion.DAO
             }
             catch (Exception e)
             {
-
+                MessageBox.Show(e.Message);
             }
-
             return dataTable;
         }
+
+
+        public DataTable buscarProducto(string texto_buscar,int numero_pagina, int numero_elementos)
+        {
+            DataTable dataTable = new DataTable();
+            try
+            {
+                conexion.AbrirConexion();
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                SqlCommand cmd = new SqlCommand("sp_paginacion_buscar_producto", conexion.ConexionSQL);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@texto_buscar", texto_buscar);
+                cmd.Parameters.AddWithValue("@numero_pagina", numero_pagina);
+                cmd.Parameters.AddWithValue("@numero_elementos", numero_elementos);
+                dataAdapter.SelectCommand = cmd;
+                dataAdapter.Fill(dataTable);
+
+                conexion.CerrarConexion();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return dataTable;
+        }
+
+
+        public int insertarProducto(string xmlProducto)
+        {
+            //int estado_insercion = -1;
+            int estado_insercion = 0;
+            try
+            {
+                conexion.AbrirConexion();
+
+                SqlCommand cmd = new SqlCommand("sp_insertar_producto", conexion.ConexionSQL);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@xml_producto", xmlProducto);
+                
+
+                SqlParameter parametro_salida = new SqlParameter("@estado", SqlDbType.TinyInt);
+                parametro_salida.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(parametro_salida);
+                cmd.ExecuteNonQuery();
+                estado_insercion = int.Parse(parametro_salida.Value.ToString());
+
+                conexion.CerrarConexion();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return estado_insercion;
+        }
+
+
+        public int actualizarProducto(string xmlProducto)
+        {
+            //int estado_insercion = -1;
+            int estado_insercion = 0;
+            try
+            {
+                conexion.AbrirConexion();
+
+                SqlCommand cmd = new SqlCommand("sp_modificar_producto", conexion.ConexionSQL);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@xml_producto", xmlProducto);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                conexion.CerrarConexion();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return estado_insercion;
+        }
+
+
+        public int eliminarProducto(int id_producto)
+        {
+            //int estado_insercion = -1;
+            int estado_eliminacion = 0;
+            try
+            {
+                conexion.AbrirConexion();
+
+                SqlCommand cmd = new SqlCommand("sp_eliminar_producto", conexion.ConexionSQL);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@id_producto", id_producto);
+
+                SqlParameter parametro_salida = new SqlParameter("@estado", SqlDbType.TinyInt);
+                parametro_salida.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(parametro_salida);
+                cmd.ExecuteNonQuery();
+                estado_eliminacion = int.Parse(parametro_salida.Value.ToString());
+                //MessageBox.Show(estado_insercion.ToString());
+                conexion.CerrarConexion();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return estado_eliminacion;
+        }
+
+
     }
 }
