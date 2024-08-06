ec50ab4b5aec73bc2a72d1a3115878ab0a9629e9
theblack <126303974+jhonleturne192005@users.noreply.github.com>
2024-08-05 20:41:39 -0500

    Update DatosConexion.cs

--- Código fuente/AccesoDatos/DatosConexion.cs
+++ Código fuente/AccesoDatos/DatosConexion.cs
@@ -8,9 +8,9 @@ namespace SistemaFacturacion.AccesoDatos
 {
     class DatosConexion
     {
-        public static string server = "aplicaciones.uteq.edu.ec, 9011";//".";
-        public static string base_datos = "FacturacionV3";
-        public static string usuario = "sa";
-        public static string clave = "ao9l3iffTySuuNygXumK4JV4W4S7vG6Y8Hgtiyxkv4Jo01XS2t";//"12345";
+        public static string server = "1";//".";
+        public static string base_datos = "1";
+        public static string usuario = "1";
+        public static string clave = "1";//"12345";
     }
 }
